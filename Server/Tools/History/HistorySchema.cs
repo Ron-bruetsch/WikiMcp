9d3840285017fe386fb446f8a4545084c1637af8
ronbruetsch <valyrious.informatics@gmail.com>
2026-03-07 18:52:58 +0100

    history tool

---
+++ Server/Tools/History/HistorySchema.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Server.Wikipedia;
+
+namespace Server.Tools.History;
+
+public enum HistoryFilter
+{
+    Reverted,
+    Anonymous,
+    Bot,
+    Minor
+}
+
+[method: JsonConstructor]
+public readonly struct HistoryInput(
+    string title,
+    uint? olderThan,
+    uint? newerThan,
+    HistoryFilter filter)
+{
+    [Description("The title of the wikipedia article")]
+    public string Title { get; } = title;
+
+    [Description("The revision ID. Returns the next 20 revisions older than the given revision ID")]
+    public uint? OlderThan { get; } = olderThan;
+
+    [Description("The revision ID. Returns the next 20 revisions newer than the given revision ID")]
+    public uint? NewerThan { get; } = newerThan;
+
+    [Description("Filter that returns only revisions with certain tags")]
+    public HistoryFilter Filter { get; } = filter;
+
+    public static HistoryInput From(IDictionary<string, JsonElement> arguments)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+public readonly struct HistoryOutput(
+    uint id,
+    PageInfo page,
+    User user,
+    uint size,
+    bool minor,
+    string timestamp,
+    int? delta,
+    string? comment)
+{
+    public uint Id { get; } = id;
+    
+    public PageInfo Page { get; } = page;
+    
+    public User User { get; } = user;
+    
+    public uint Size { get; } = size;
+    
+    public bool Minor { get; } = minor;
+    
+    public string Timestamp { get; } = timestamp;
+    
+    public int? Delta { get; } = delta;
+    
+    public string? Comment { get; } = comment;
+    
+    public HistoryOutput(Revision model) : this(
+        model.Id,
+        model.Page,
+        model.User,
+        model.Size,
+        model.Minor,
+        model.Timestamp,
+        model.Delta,
+        model.Comment)
+    {
+        
+    } 
+}
---
+++ Server/Tools/History/HistoryTool.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Text.Json;
+using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+using Server.Errors;
+using Server.Wikipedia;
+
+namespace Server.Tools.History;
+
+public readonly struct HistoryErrorContext : IErrorContext
+{
+    public static (string, string?) FromStatusCode(ErrorResponseObject model)
+    {
+        switch (model.HttpCode)
+        {
+            case 400:
+                throw new NotImplementedException();
+            case 404:
+                return ("Title or revision not found", null);
+            default:
+                throw new NotSupportedException(
+                    $"Handling error code {model.HttpCode} for history tool is not supported.");
+        }
+    }
+}
+
+public static class HistoryTool
+{
+    public const string Name = "get-article-history";
+
+    private static readonly JsonElement InputSchema = Helper.ToJsonSchema(typeof(HistoryInput));
+    private static readonly JsonElement OutputSchema = Helper.ToJsonSchema(typeof(McpOutput<IEnumerable<HistoryOutput>>));
+    
+    public static async ValueTask<CallToolResult> RunAsync(
+        HttpClient httpClient,
+        RequestContext<CallToolRequestParams> requestContext,
+        CancellationToken ct)
+    {
+        HistoryInput input = HistoryInput.From(requestContext.Params!.Arguments!);
+        StringBuilder urlBuilder = new($"page/{input.Title}/history?filter={input.Filter.ToString().ToLower()}");
+
+        if (input.OlderThan is { } ot)
+        {
+            urlBuilder.Append($"&older_than={ot}");    
+        }
+        
+        if (input.NewerThan is { } nt)
+        {
+            urlBuilder.Append($"&newer_than={nt}");
+        }
+        
+        string url = urlBuilder.ToString();
+        
+        using HttpResponseMessage response = await httpClient.GetAsync(url, ct);
+        
+        if (!response.IsSuccessStatusCode)
+        {
+            throw await WikipediaException.FromAsync<HistoryErrorContext>(response, ct);     
+        }
+
+        Paged<Revision> page = await response.Content.ReadFromJsonAsync<Paged<Revision>>(ct);
+        IEnumerable<HistoryOutput> output = page.Revisions!.Select(x => new HistoryOutput(x));
+        
+        return new CallToolResult()
+        {
+            IsError = false,
+            StructuredContent = JsonSerializer.SerializeToElement(output)
+        };
+    }
+
+    public static Tool Tool() =>
+        new()
+        {
+            Name = Name,
+            Description = "Retrieves the change history of a specific Wikipedia article",
+            Title = "Get Article History",
+            InputSchema = InputSchema,
+            OutputSchema = OutputSchema,
+        };
+}
